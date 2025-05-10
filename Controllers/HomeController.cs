f5ea731c35174d1fd0da540f9a2b0dcd16efbb11
maximum2974 <1291185069@qq.com>
2025-05-10 16:24:22 +0800

    feat: 项目初始化

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Mvc;
+using Maximum_Portfolio.Models;
+using System.Diagnostics;
+
+namespace Maximum_Portfolio.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    namespace Portfolio.Controllers
+    {
+        public class HomeController : Controller
+        {
+            public IActionResult Index()
+            {
+                return View();
+            }
+
+            public IActionResult Projects()
+            {
+                return View();
+            }
+
+            public IActionResult Resume()
+            {
+                return View();
+            }
+
+            public IActionResult Contact()
+            {
+                return View();
+            }
+
+            [HttpPost]
+            public IActionResult Contact(ContactFormModel model)
+            {
+                if (ModelState.IsValid)
+                {
+                    // Handle form submission, e.g., send email or save data to the database
+                    ViewBag.Message = "Thank you for reaching out!";
+                }
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/ContactFormModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Maximum_Portfolio.Models
+{
+    public class ContactFormModel
+    {
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Message { get; set; }
+    }
+}
